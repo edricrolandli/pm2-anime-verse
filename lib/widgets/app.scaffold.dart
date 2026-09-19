7a9a62d4261e810be8f76b7f99ba2fa947d891b6
edricrolandli <edricrolandli@gmail.com>
2026-09-19 23:42:39 +0700

    feat(widgets): add app scaffold with gradient background

---
+++ lib/widgets/app.scaffold.dart
@@ -0,0 +1,25 @@
+import 'package:anime_verse/widgets/gradient_background.dart';
+import 'package:flutter/material.dart';
+
+class AppScaffold extends StatelessWidget {
+  final PreferredSizeWidget? appBar;
+  final Widget body;
+  
+  
+  const AppScaffold ({
+    super.key,
+    this.appBar,
+    required this.body
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GradientBackground(
+        child: Scaffold(
+            body: body,
+            appBar: appBar,
+            backgroundColor: Colors.transparent,
+        )
+    );
+  }
+}
